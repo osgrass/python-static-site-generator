ab2f3ce58f04b79d9ca1c0f1523fbef103bce144
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-03 17:56:27 -0400

    sds
    test

---
+++ ssg/site.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+
+print("hello")
+
+class Site:
+    def __init__(self, source, dest, parsers=None):
+        self.source = Path(source)
+        self.dest = Path(dest)
+        self.parsers = parsers or []
+        
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+        
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser 
+            
+    def run_parser(self, path):
+        parser = self.load_pareser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            print("not implemented")           
+        
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)     
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+                
+        print(path)
+                   
