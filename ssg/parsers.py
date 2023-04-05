1eea19bb2159d52fb3ac0c27d566739a956296c6
osgrass <64144934+osgrass@users.noreply.github.com>
2023-04-05 16:12:51 -0400

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,7 +5,7 @@ import sys
 
 from docutils.core import publish_parts
 from markdown import markdown
-from content import Content
+from ssg.content import Content
 
 class Parser:
     # extensions List[str]
@@ -53,6 +53,6 @@ class ReStructuredTextParser(Parser):
     
      def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = publish_parts(content.body, writer_name="html")
+        html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write("\x1b[1;23m{} converted to HTML medataa:{}\n".format(path.name, content))
